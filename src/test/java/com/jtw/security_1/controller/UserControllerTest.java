20fb4f934bf68605dfb6c3d9cb960fa90d46dee8
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-10 21:09:29 +0900

    유저 Join, 중복오류 테스트

---
+++ src/test/java/com/jtw/security_1/controller/UserControllerTest.java
@@ -0,0 +1,58 @@
+package com.jtw.security_1.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jtw.security_1.domain.User;
+import com.jtw.security_1.domain.dto.UserJoinRequest;
+import com.jtw.security_1.service.UserService;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+        String userName = "BLoHny";
+        String password = "1234";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))//UserJoin에 담은값 JSON으로 변환
+                .andDo(print()) //요청 응답 출력
+                .andExpect(status().isOk()); //Is it 200?
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - userName 중복")
+    void join_fail() throws Exception {
+        String username = "BLoHny";
+        String password = "1234";
+
+        mockMvc.perform(post("/api/v1/users/join")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(new UserJoinRequest(username, password))))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+}
