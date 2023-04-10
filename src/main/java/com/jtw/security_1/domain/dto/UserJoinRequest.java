3324847d4c8b222074fbf3dc691b84193c54380e
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-10 21:09:10 +0900

    Converter Json변환 오류 수정

--- src/main/java/com/jtw/security_1/domain/dto/UserJoinRequest.java
+++ src/main/java/com/jtw/security_1/domain/dto/UserJoinRequest.java
@@ -1,10 +1,13 @@
 package com.jtw.security_1.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class UserJoinRequest {
 
     private String userName;
