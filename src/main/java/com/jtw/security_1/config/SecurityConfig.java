4314b7607acf7d7c474198fb5708a225ce27de9a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-12 21:04:56 +0900

    Config 수정

--- src/main/java/com/jtw/security_1/config/SecurityConfig.java
+++ src/main/java/com/jtw/security_1/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.jtw.security_1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -17,11 +20,11 @@ public class SecurityConfig {
                 .cors().and() //도메인이 다를때 허용
                 .authorizeHttpRequests()
                 //인가 정책
-                .requestMatchers("/api/**").permitAll()
                 .requestMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/reviews").authenticated()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //JWT를 쓰는 경우
                 .and()
                 .build();
     }
