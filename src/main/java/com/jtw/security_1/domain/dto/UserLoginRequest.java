d2eaec69adcdf2aede27879e7e2d199ff1edd54a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-12 20:23:26 +0900

    오류 수정(어노테이션 수정)

--- src/main/java/com/jtw/security_1/domain/dto/UserLoginRequest.java
+++ src/main/java/com/jtw/security_1/domain/dto/UserLoginRequest.java
@@ -2,12 +2,13 @@ package com.jtw.security_1.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class UserLoginRequest {
 
     private String userName;
     private String password;
-
 }
