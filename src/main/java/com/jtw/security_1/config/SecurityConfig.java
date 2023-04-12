29409e9698e8f02970b349a1b91dcb3a8cd09adf
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-12 21:16:55 +0900

    토큰 검사

--- src/main/java/com/jtw/security_1/config/SecurityConfig.java
+++ src/main/java/com/jtw/security_1/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.jtw.security_1.config;
 
+import com.jtw.security_1.domain.User;
+import com.jtw.security_1.filter.JwtFilter;
+import com.jtw.security_1.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,10 +12,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserService userService;
+
+    @Value("${jwt.token.secret}")
+    private String key;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception { //DSL 사용 보안구성
@@ -26,6 +38,7 @@ public class SecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS) //JWT를 쓰는 경우
                 .and()
+                .addFilterBefore(new JwtFilter(userService, key), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
