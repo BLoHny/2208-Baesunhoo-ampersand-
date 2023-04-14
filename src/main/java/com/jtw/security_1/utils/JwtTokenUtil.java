9ca27fd21ded602987745feaeed0ee4f69102304
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-14 20:29:29 +0900

    ACCESS, REFRESH

--- src/main/java/com/jtw/security_1/utils/JwtTokenUtil.java
+++ src/main/java/com/jtw/security_1/utils/JwtTokenUtil.java
@@ -3,10 +3,18 @@ package com.jtw.security_1.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.AllArgsConstructor;
 
 import java.util.Date;
 
 public class JwtTokenUtil  {
+
+    @AllArgsConstructor
+    private enum TokenType {
+        ACCESS_TOKEN,
+        REFRESH_TOKEN;
+    }
+
     public static String createToken(String userName, String key, long expireTimeMs) {
         Claims claims = Jwts.claims(); //일종의 Map
         claims.put("userName", userName);
