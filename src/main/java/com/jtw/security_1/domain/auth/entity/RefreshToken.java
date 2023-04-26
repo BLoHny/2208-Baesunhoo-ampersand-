d53e95242f58a16f1ee333e762ed3afe2fd2c558
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-26 21:14:25 +0900

    Service - product

---
+++ src/main/java/com/jtw/security_1/domain/auth/entity/RefreshToken.java
@@ -0,0 +1,27 @@
+package com.jtw.security_1.domain.auth.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@RedisHash(value = "refreshToken")
+public class RefreshToken {
+
+    @Id
+    private String userName;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive
+    private long expiredAt;
+
+    public void exchangeRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
