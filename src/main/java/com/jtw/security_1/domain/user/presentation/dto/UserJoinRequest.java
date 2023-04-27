50f8a918b30a9241b25ba29d08e036bb1ceff41b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-27 21:17:24 +0900

    Dto 수정

--- src/main/java/com/jtw/security_1/domain/user/presentation/dto/UserJoinRequest.java
+++ src/main/java/com/jtw/security_1/domain/user/presentation/dto/UserJoinRequest.java
@@ -1,5 +1,6 @@
 package com.jtw.security_1.domain.user.presentation.dto;
 
+import com.jtw.security_1.domain.user.entity.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
@@ -12,4 +13,5 @@ public class UserJoinRequest {
 
     private String userName;
     private String password;
+    private Role role;
 }
