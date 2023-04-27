e88b140fa47a0641770291f95e243fea84211079
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-27 21:17:55 +0900

    service 수정

--- src/main/java/com/jtw/security_1/domain/user/service/UserService.java
+++ src/main/java/com/jtw/security_1/domain/user/service/UserService.java
@@ -4,15 +4,14 @@ import com.jtw.security_1.domain.auth.entity.RefreshToken;
 import com.jtw.security_1.domain.auth.presentation.LoginResponse;
 import com.jtw.security_1.domain.auth.repostiory.RefreshTokenRepository;
 import com.jtw.security_1.domain.user.entity.User;
+import com.jtw.security_1.domain.user.presentation.dto.UserJoinRequest;
 import com.jtw.security_1.domain.user.presentation.dto.UserLoginRequest;
+import com.jtw.security_1.domain.user.repository.UserRepository;
 import com.jtw.security_1.global.exception.AppException;
 import com.jtw.security_1.global.exception.ErrorCode;
-import com.jtw.security_1.domain.user.repository.UserRepository;
 import com.jtw.security_1.global.security.jwt.TokenProvider;
 import com.jtw.security_1.global.security.jwt.properties.JwtProperties;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,17 +28,18 @@ public class UserService {
     private final JwtProperties jwtProperties;
 
     @Transactional(rollbackFor = Exception.class)
-    public String join(String userName, String password) {
+    public String join(UserJoinRequest request) {
 
         //중복 CHECK -> USERNAME
-        userRepository.findByUserName(userName)
+        userRepository.findByUserName(request.getUserName())
                 .ifPresent(user -> {
-                    throw new AppException(ErrorCode.USERNAME_DUPLICATED, userName + "는 이미 있습니다.");
+                    throw new AppException(ErrorCode.USERNAME_DUPLICATED, request.getUserName() + "는 이미 있습니다.");
                 });
 
         User user = User.builder()
-                .userName(userName)
-                .password(passwordEncoder.encode(password))
+                .userName(request.getUserName())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
                 .build();
         //저장
         userRepository.save(user);
@@ -47,19 +47,21 @@ public class UserService {
         return "SUCCESS";
     }
 
-    public LoginResponse execute(UserLoginRequest signinRequest) {
+    public LoginResponse execute(UserLoginRequest signInRequest) {
 
         User user = userRepository
-                .findByUserName(signinRequest.getUserName())
+                .findByUserName(signInRequest.getUserName())
                 .orElseThrow(()->new RuntimeException("없는 유저 입니다."));
 
-        if (!passwordEncoder.matches(signinRequest.getPassword(), user.getPassword())) {
+        if (!passwordEncoder.matches(signInRequest.getPassword(), user.getPassword())) {
             throw new RuntimeException("패스워드가 일치 하지 않음");
         }
 
-        String accessToken = tokenProvider.generatedAccessToken(loginRequest.getUserName());
-        String refreshToken = tokenProvider.generatedRefreshToken(loginRequest.getUserName());
-        RefreshToken refreshTokenEntity = new RefreshToken(loginRequest.getUserName(), refreshToken,tokenProvider.getREFRESH_TOKEN_EXPIRE_TIME());
+
+        String accessToken = tokenProvider.generatedAccessToken(signInRequest.getUserName());
+        String refreshToken = tokenProvider.generatedRefreshToken(signInRequest.getUserName());
+
+        RefreshToken refreshTokenEntity = new RefreshToken(signInRequest.getUserName(), refreshToken,tokenProvider.getREFRESH_TOKEN_EXPIRE_TIME());
 
         refreshTokenRepository.save(refreshTokenEntity);
 
