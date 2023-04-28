942a5c197d00d353c0d3b0bcebe805e896868d19
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-28 22:00:01 +0900

    List Service

--- src/main/java/com/jtw/security_1/domain/product/service/ProductListService.java
+++ src/main/java/com/jtw/security_1/domain/product/service/ProductListService.java
@@ -1,8 +1,13 @@
 package com.jtw.security_1.domain.product.service;
 
+import com.jtw.security_1.domain.product.entity.Product;
 import com.jtw.security_1.domain.product.repositories.ProductRepositories;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -10,7 +15,16 @@ public class ProductListService {
 
     private final ProductRepositories productRepositories;
 
-    public void ProductList() {
+    public List<Map<String, Object>> ProductList() {
+        List<Product> productList = productRepositories.findAll();
 
+        return productList.stream()
+                .map(product -> {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("name", product.getProductName());
+                    map.put("price", product.getPrice());
+                    return map;
+                })
+                .toList();
     }
 }
