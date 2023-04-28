64f95996cfe00039d50a2a0bbefd6e6b8eeb9e9b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-28 20:51:09 +0900

    에러 종류 분류

--- src/main/java/com/jtw/security_1/global/exception/ErrorCode.java
+++ src/main/java/com/jtw/security_1/global/exception/ErrorCode.java
@@ -10,8 +10,15 @@ public enum ErrorCode {
 
     USERNAME_DUPLICATED(HttpStatus.CONFLICT, ""),
     USERNAME_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
+    TOKEN_IS_EXPIRED(HttpStatus.UNAUTHORIZED, ""),
+    TOKEN_NOT_VALID(HttpStatus.UNAUTHORIZED, "" ),
+    PRODUCT_NOTFOUND(HttpStatus.NOT_FOUND, ""),
+    PRODUCT_DUPLICATED(HttpStatus.CONFLICT, ""),
+    PRODUCT_INVALID_USERID(HttpStatus.UNAUTHORIZED, "");
+
 
     private final HttpStatus httpStatus;
     private final String message;
+
 }
