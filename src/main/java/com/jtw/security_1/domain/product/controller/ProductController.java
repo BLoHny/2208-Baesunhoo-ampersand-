fcaf01ea026a385a371fd0ff7fe1da42576098af
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-28 15:45:30 +0900

    자신의 상품 목록, 상품 디테일 서비스

--- src/main/java/com/jtw/security_1/domain/product/controller/ProductController.java
+++ src/main/java/com/jtw/security_1/domain/product/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.jtw.security_1.domain.product.presentation.ProductJoinRequest;
 import com.jtw.security_1.domain.product.repositories.ProductRepositories;
 import com.jtw.security_1.domain.product.service.CreateProductService;
 import com.jtw.security_1.domain.product.service.DeleteProductService;
+import com.jtw.security_1.domain.product.service.ProductDetailService;
+import com.jtw.security_1.domain.product.service.ProductMeListService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,8 @@ public class ProductController {
     private final ProductRepositories productRepositories;
     private final CreateProductService productService;
     private final DeleteProductService deleteProductService;
+    private final ProductMeListService productMeListService;
+    private final ProductDetailService productDetailService;
 
     @GetMapping("/list")
     public ResponseEntity<List<Product>> ProductList() {
@@ -37,5 +41,18 @@ public class ProductController {
     public ResponseEntity<?> ProductDelete(@RequestBody ProductDeleteRequest deleteRequest) {
         deleteProductService.DeleteProduct(deleteRequest);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/me/list")
+    public List<Product> productMeList() {
+        List<Product> meList = productMeListService.ProductMeList();
+
+        return new ResponseEntity<>(meList, HttpStatus.OK).getBody();
+    }
+
+    @GetMapping("/detail")
+    public Product productDetail(@RequestParam Long productId) {
+        Product product =  productDetailService.productDetail(productId);
+        return new ResponseEntity<>(product, HttpStatus.OK).getBody();
     }
 }
