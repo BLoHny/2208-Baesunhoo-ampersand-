6d727eb1c0a9009947a6b7849fc8edefb31643d7
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-28 15:46:48 +0900

    OrderRepository

---
+++ src/main/java/com/jtw/security_1/domain/order/repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.jtw.security_1.domain.order.repository;
+
+import com.jtw.security_1.domain.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findByUserId(Long userId);
+}
