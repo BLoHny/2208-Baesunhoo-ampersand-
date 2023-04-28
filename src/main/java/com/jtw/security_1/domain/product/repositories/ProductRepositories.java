25599878920dda335773561aeef075c3d37c55cd
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-04-28 15:45:46 +0900

    Repository 수정

--- src/main/java/com/jtw/security_1/domain/product/repositories/ProductRepositories.java
+++ src/main/java/com/jtw/security_1/domain/product/repositories/ProductRepositories.java
@@ -4,10 +4,17 @@ import com.jtw.security_1.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepositories extends JpaRepository<Product, Long> {
 
     Boolean existsByProductName(String productName);
 
     Product findByProductName(String productName);
+
+    Product findByProductId(Long productId);
+
+    List<Product> findByUserId(Long userId);
+
 }
